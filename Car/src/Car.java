2ef48397d3d65ca42d0f25ef73c3defac9545939
sihle <sihlembhem@gmail.com>
2021-03-16 19:20:00 +0200

    Car dealership

---
+++ Car/src/Car.java
@@ -0,0 +1,44 @@
+/**
+/**
+ * @author Sihle
+ *
+ */
+public class Car {
+	
+	 String Make;
+	 String Model;
+	 String ModelDetails;
+	 
+	 //public String Make() {
+		 
+	 
+	//	 return Make;	 
+	 //}
+	 
+	 //public String getModel() {
+		// return Model;	 
+	 //}
+	 
+	// public String getModelDetails() {
+		// return ModelDetails;	 
+	 //}
+	 
+	 
+	 public Car(String Make, String Model, String ModelDetailsGiven) {
+		 Make = Make;
+		 Model =  Model;
+		 ModelDetails = ModelDetails;	 
+	 }
+}
+	 
+	 
+
+
+
+
+
+
+
+
+
+
